19fb2d5869ccbfe51253fd75ed5146180011f7d1
ferdiazn <162004124+ferdiazn@users.noreply.github.com>
2024-06-14 21:15:05 +0700

    Update 1_Dataframe_Demo_(No_Caching).py

--- pages/1_Dataframe_Demo_(No_Caching).py
+++ pages/1_Dataframe_Demo_(No_Caching).py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-%matplotlib inline
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import statsmodels.api as sm
