efe29ffce17bbd929698f5d1e50a2c9a0a3da8b5
ferdiazn <162004124+ferdiazn@users.noreply.github.com>
2024-06-14 20:24:13 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+)
+
+st.markdown("# Welcome to Streamlit! 👋")
+
+st.markdown(
+    """
+    Welcome to your first streamlit app!
+
+    **👈 Select any pages from the sidebar** to see some examples
+    of what we can achieve in Streamlit!
+
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+        
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
