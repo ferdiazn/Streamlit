94109ee49bbe16fd9f9929fbe159aea44aa8622f
ferdiazn <162004124+ferdiazn@users.noreply.github.com>
2024-06-14 20:42:33 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -28,8 +28,8 @@ st.markdown(
 
 genre = st.radio(
     "What's your favorite movie genre",
-    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
-    index=None,
+    ["A", "B", "C"],
+    index="A",
 )
 
 st.write("You selected:", genre)
