f2371021540f1869271fd3b999941a4efc39e923
ferdiazn <162004124+ferdiazn@users.noreply.github.com>
2024-06-14 20:40:26 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -29,9 +29,7 @@ st.markdown(
 genre = st.radio(
     "What's your favorite movie genre",
     [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
-    captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+    index=None,
+)
 
-if genre == ":rainbow[Comedy]":
-    st.write("You selected comedy.")
-else:
-    st.write("You didn't select comedy.")
+st.write("You selected:", genre)
